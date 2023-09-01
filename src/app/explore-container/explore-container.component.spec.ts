abb3168948a88f8b1211bd51b0c467f2bf0f805e
João Subtil <johndotnet.contact@gmail.com>
2023-09-01 07:47:40 -0300

    Limpando e organizando alguns trechos do código

--- src/app/explore-container/explore-container.component.spec.ts
+++ src/app/explore-container/explore-container.component.spec.ts
@@ -1,13 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ExploreContainerComponent } from './explore-container.component';
+import { BrowserTestingModule } from '@angular/platform-browser/testing';
 
 describe('ExploreContainerComponent', () => {
   let component: ExploreContainerComponent;
   let fixture: ComponentFixture<ExploreContainerComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule().compileComponents();
+    await TestBed.configureTestingModule(new Object()).compileComponents();
 
     fixture = TestBed.createComponent(ExploreContainerComponent);
     component = fixture.componentInstance;
