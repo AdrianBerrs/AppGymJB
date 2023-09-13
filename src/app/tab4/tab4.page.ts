8064ab8d3c57a3202d9f0cf03f3f647aa2c5a805
AdrianBerrs <adrian_santosbernardo@outlook.com>
2023-09-13 01:43:36 -0300

    ajuste_front_nova_avaliacao

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { AvaliacoesService} from '../avaliacoes.service';
-import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-tab4',
   templateUrl: 'tab4.page.html',
   styleUrls: ['tab4.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent,FormsModule,HttpClientModule],
+  imports: [
+    IonicModule,
+    ExploreContainerComponent,
+    FormsModule,
+  ],
+  providers: [Storage]
 })
 
 export class Tab4Page {
